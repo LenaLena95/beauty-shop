9774f7ada8bb307bb4cb23f853da7d65ae7d19aa
Itstep <seleznev_k@itstep.org>
2019-06-23 12:11:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,7 @@ namespace BeautyShop
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
+        
         /// </summary>
         [STAThread]
         static void Main()
